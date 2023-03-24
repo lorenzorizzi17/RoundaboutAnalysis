31edecdfe5cdf934f76b80ca32c6d75dd28e201e
Lorenzo Rizzi <lorenzorizzi17@gmail.com>
2023-03-24 17:03:31 +0100

    First working database

--- histo.cpp
+++ histo.cpp
@@ -2,7 +2,9 @@
 #include<fstream>
 
 void histo() {
-    TH1F* histo = new TH1F("Histo", "Histo", 40,0,10);
+    TH1F* histo = new TH1F("Flux in: 9", "Mean n. of cars in roads", 40,0,10);
+    TCanvas* c1 = new TCanvas("","",800,500);
+    c1->cd(1);
     std::ifstream myfile ("/home/lorenzo17/Roundabout2/data.dat",std::ifstream::in);
 
     while(myfile.good()){
@@ -12,7 +14,9 @@ void histo() {
         histo->Fill(x);
     }
     myfile.close();
+    
     histo->Draw();
+    
     std::cout << "\nLa media è: " << histo->GetMean() << '\n'; 
-
+    c1->Print("histo_9_6.pdf");
 }
--- mainng.cpp
+++ mainng.cpp
@@ -24,7 +24,7 @@ int main() {
     
     //simulation parameters
     int const N_ROADS = 4;
-    const double rates[N_ROADS] = {8,8,8,8};  //calcola flusso di ingresso
+    const double rates[N_ROADS] = {9,9,9,9};  //calcola flusso di ingresso
     const double amplificationTransfer = 1.2;
     const double v_road = 3;
     const double v_rbout = 2.85;
@@ -50,8 +50,8 @@ int main() {
         throw std::runtime_error{"Modificare il numero di strade."};
     }
 
-    double b =0;
-    while (true) {
+    int b =0;
+    while (b<1E6) {
         for (auto it = roads.begin(); it != roads.end(); ++it) {
           std::sort(roundabout.carrbout().begin(),roundabout.carrbout().end(), myfunction);
           (*it).newcar_rd(true, it->rate(), n_max_car, offset);
@@ -75,13 +75,19 @@ int main() {
           //std::cerr << "Macchine nella strada a " << (it->angle())*180/M_PI << " gradi: " << (it->carin()).size() << '\n';
           mean += (it->carin()).size();
         }
-        std::cerr << mean/N_ROADS<<'\n';
+        //std::cerr << mean/N_ROADS<<'\n';
         myfile << ((mean/N_ROADS)) <<std::endl;
         myfile2 << b << " " << ((mean/N_ROADS)) << std::endl;
         b++;
+        if (b%10000 ==0)
+      {
+        std::cerr << (b/1000) << '\n';
+      }
       }
       myfile.close();
       myfile2.close();
+      
+      
     }
     catch (std::runtime_error const& e) {
       std::cerr << e.what() << '\n';
